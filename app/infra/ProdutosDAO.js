2c739be028b1925018f2aca9c711f0951eb15bcd
Robson Amorim <rob.s.amorim@gmail.com>
2017-08-10 00:55:39 -0300

    Adiciona body parser para receber o json dos dados da tela e feito cadstro de livro

--- app/infra/ProdutosDAO.js
+++ app/infra/ProdutosDAO.js
@@ -6,6 +6,10 @@ ProdutosDAO.prototype.todos =  function(callback){
     this._connection.query('select * from livros', callback)
 }
 
+ProdutosDAO.prototype.salvar = function(produto, callback){
+    this._connection.query('insert into livros set ?', produto, callback);
+}
+
 module.exports = function(){
         return ProdutosDAO;
 }   
--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,15 +1,32 @@
 
 module.exports = function(app){
 
-    app.get('/produtos', function(req, res){
+    var listaLivros =  function(req, res){
         
         var connection = app.infra.connectionFactory();
-        var produtosBanco = new app.infra.ProdutosDAO(connection);
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
         
-        produtosBanco.todos(function(err, results){
+        produtosDAO.todos(function(err, results){
             res.render('produtos/lista', {lista: results});
         });
-        
         connection.end();
+    };
+
+    app.get('/produtos/cadastro', function(req, res){
+        res.render('produtos/form')
+    });
+
+    app.get('/produtos', listaLivros);
+
+    app.post('/produtos', function(req, res){
+        
+        var produto = req.body;
+
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        
+        produtosDAO.salvar(produto, function(err, result){
+            res.redirect('/produtos');
+        });
     });
 }
